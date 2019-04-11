295f3a5e48f12ee4eac07159221f9cf9aaca569c
Lucas Dachman <lucas.dachman@gmail.com>
2019-04-11 13:27:16 -0600

    remove build from .gitignore

---
+++ build/precache-manifest.fd9f660f09e369d879a48a6cd3d32fe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "baff65a58ba439821995",
+    "url": "/static/css/main.6bf467e0.chunk.css"
+  },
+  {
+    "revision": "baff65a58ba439821995",
+    "url": "/static/js/main.5a2308d5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3aff95978ae972d2a8e6",
+    "url": "/static/css/2.b9c673a7.chunk.css"
+  },
+  {
+    "revision": "3aff95978ae972d2a8e6",
+    "url": "/static/js/2.f0d01fa9.chunk.js"
+  },
+  {
+    "revision": "dce297d6b80fd7b2cd4abee1578d42dc",
+    "url": "/index.html"
+  }
+];
